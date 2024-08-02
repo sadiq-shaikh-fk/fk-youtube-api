adff1a23f1250b2134e0cf313fa044fe32039ea8
Sadiq Shaikh <sadiq.shaikh@famekeeda.com>
2024-08-02 14:04:52 +0530

    updated 200

--- channel_id_route.py
+++ channel_id_route.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def get_channel_details(channel_id):
     try:
         details = get_channel_details_from_id(channel_id)
-        return jsonify(details), 400
+        return jsonify(details), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
