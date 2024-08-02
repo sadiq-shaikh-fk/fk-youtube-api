296a000691b9279ded38991de474af0f3af11acb
Sadiq Shaikh <sadiq.shaikh@famekeeda.com>
2024-08-02 13:48:45 +0530

    added port no in code

--- channel_id_route.py
+++ channel_id_route.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 
 @app.route('/channel/<string:channel_id>', methods = ['GET'])
 def get_channel_details(channel_id):
-    details = get_channel_details_from_id(channel_id)
-    return jsonify(details)
+    try:
+        details = get_channel_details_from_id(channel_id)
+        return jsonify(details), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
