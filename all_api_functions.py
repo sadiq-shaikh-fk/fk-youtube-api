82229d74a9f2fbc22b0a0b0827165099061eca4f
Sadiq Shaikh <sadiq.shaikh@famekeeda.com>
2024-08-28 18:22:33 +0530

    added video API

--- all_api_functions.py
+++ all_api_functions.py
@@ -17,9 +17,11 @@ current_key_index = 0
 max_requests_per_key = 1000  # Set a threshold for when to switch keys
 request_count = 0
 
+# ------------------------- LOGIC FOR RE-BUILDING FOR NEW API KEY -------------------------
 async def build_youtube_service(api_key):
     return build(api_service_name, api_version, developerKey=api_key)
 
+# ------------------------- LOGIC FOR CHANNEL EXTRACTION -------------------------
 async def get_channel_details_from_id(channel_id):
     global current_key_index, request_count
 
@@ -75,6 +77,7 @@ async def get_channel_details_from_id(channel_id):
         'input_channel_id' : channel_id
     }
 
+# ------------------------- LOGIC FOR PLAYLIST_ID EXTRACTION -------------------------
 async def get_video_id_from_playlist(playlist_id, pageToken=None):
     global current_key_index, youtube
 
@@ -112,33 +115,105 @@ async def get_video_id_from_playlist(playlist_id, pageToken=None):
     }
 
 
-async def get_video_details(video_id, pageToken=None):
+# ------------------------- LOGIC FOR VIDEOS EXTRACTION -------------------------
+async def get_all_video_details(video_id_strings):
     global current_key_index, youtube
+    response_of_all_video_ids = []
+    response_of_all_video_ids_final = []
+
+    # making chunks of 50 string from srting all video_id
+    video_id_strings_chunked = await video_id_list_to_string(video_id_strings)
 
     async with aiohttp.ClientSession() as session:
         while True:
-            youtube = await build_youtube_service(api_keys[current_key_index])
-            request = youtube.videos().list(
-            part="id,snippet,status,contentDetails",
-            id = video_id
-            )
-            # request.uri = fix_url(str(request.uri))     # Build the URL from the request
             try:
-                # request execution
-                response = request.execute() # 1 credit used
-                break
+                youtube = await build_youtube_service(api_keys[current_key_index])
+                for each_chunk in video_id_strings_chunked:
+                    request = youtube.videos().list(
+                    part="contentDetails,id,player,snippet,statistics,status,topicDetails",
+                    id = str(each_chunk)
+                    #fields = "items(id,snippet(title,description,publishedAt,thumbnails(high),localized,country),statistics(viewCount,subscriberCount,videoCount),status(privacyStatus,madeForKids),brandingSettings(image,channel),contentDetails(relatedPlaylists(uploads)))"
+                    )
+                    try:
+                        # request execution
+                        response = request.execute() # 1 credit used
+                        items = response.get('items', [{}])
+                        response_of_all_video_ids.append(items)
+                        
+                        for each_response in response_of_all_video_ids:
+                            for item in each_response:
+                                try:
+                                    response_of_all_video_ids_final.append(item)
+                                except Exception as e:
+                                    print(f'except occured{e}')
+                        break
 
-            except HttpError as e:
-                if e.resp.status == 403 and 'exceeded' in e.reason:
-                    print(f"Quota exceeded for key: {api_keys[current_key_index]}. Trying next key...")
-                    current_key_index = (current_key_index + 1) % len(api_keys)
-                    await asyncio.sleep(1)  # Avoid too rapid retries
-                else:
-                    return {"error": f"API Error: {str(e)}"}
+                    except HttpError as e:
+                        if e.resp.status == 403 and 'exceeded' in e.reason:
+                            print(f"Quota exceeded for key: {api_keys[current_key_index]}. Trying next key...")
+                            current_key_index = (current_key_index + 1) % len(api_keys)
+                            #await asyncio.sleep(1)  # Avoid too rapid retries
+                            
+                            print(f"error - API Error: {str(e)}")
+                        elif e.resp.status == 400 and 'No filter' in e.reason:
+                            print('no filter was selected.. (channel did not gave uploadPlaylistId)')
+                            print()
+                            #return [None] * 5
+
+                        else:
+                            print('API URL invalid or other error:', e)
+                            print('------------------------------------------------------------------------------------------------')
+                            #return [None] * 5
+                    except Exception as e:
+                        print('Some error - ', e)
+            
+            except Exception as e:
+                print('Some error at query Building', e)
+
+            return {
+                    "items" : response_of_all_video_ids_final
+                    }
+        
+# ----------- LOGIC LIST TO STRING -----------
+async def video_id_list_to_string(video_ids_lst):
+    video_id_strings = []
+    total_video_ids = len(video_ids_lst)          # storing the total videos count to create chunks
+
+    for i in range(0, total_video_ids, 50):
+        chunk = video_ids_lst[i:i+50]             # Get the current chunk of 50 (or fewer if at the end)
+        video_id_str = ','.join(chunk)            # Convert the chunk to a comma-separated string without spaces        
+        video_id_strings.append(video_id_str)     # Append the string to the new list
+    return video_id_strings
+
+
+# ---------------------------------------- OLD CODE ---------------------------------------- 
+# async def get_video_details(video_id, pageToken=None):
+#     global current_key_index, youtube
+
+#     async with aiohttp.ClientSession() as session:
+#         while True:
+#             youtube = await build_youtube_service(api_keys[current_key_index])
+#             request = youtube.videos().list(
+#             part="id,snippet,status,contentDetails",
+#             id = video_id
+#             )
+#             # request.uri = fix_url(str(request.uri))     # Build the URL from the request
+#             try:
+#                 # request execution
+#                 response = request.execute() # 1 credit used
+#                 break
+
+#             except HttpError as e:
+#                 if e.resp.status == 403 and 'exceeded' in e.reason:
+#                     print(f"Quota exceeded for key: {api_keys[current_key_index]}. Trying next key...")
+#                     current_key_index = (current_key_index + 1) % len(api_keys)
+#                     await asyncio.sleep(1)  # Avoid too rapid retries
+#                 else:
+#                     return {"error": f"API Error: {str(e)}"}
     
-    video_details_clean = response.get('items', [{}])
+#     video_details_clean = response.get('items', [{}])
 
-    return{
-        'vide_title' : video_details_clean.get("snippet", {}).get("title", ""),
-        'input_video_id' : video_id
-    }
+#     return{
+#         'vide_title' : video_details_clean.get("snippet", {}).get("title", ""),
+#         'input_video_id' : video_id
+#     }
